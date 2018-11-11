560efd2543da18885ac6ef5fa3ef05e01971a921
Nick <plus0bit@gmail.com>
2018-11-11 21:15:08 +0300

    Keep x variable in local storage

--- public/app.js
+++ public/app.js
@@ -1,9 +1,11 @@
 
 function something(){
     
-    let x = window.localStorage.getItem('n');
+    let x = window.localStorage.getItem('nn');
+
+    x = x * 1 + 1;
     
-    window.localStorage.setItem('n', 777);
+    window.localStorage.setItem('nn', x);
 
     alert(x);
 }
