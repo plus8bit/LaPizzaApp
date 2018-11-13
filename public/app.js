1031e0c6efe2b35e137c7ffb7205b54522b65681
Nick <plus0bit@gmail.com>
2018-11-13 14:41:53 +0300

    Add number_of_items function

--- public/app.js
+++ public/app.js
@@ -17,4 +17,24 @@ function add_to_cart(id){
     var x = window.localStorage.getItem(key);
     x = x * 1 + 1;
     window.localStorage.setItem(key, x);
+
+    //show number of items in the cart
+    alert('Items in your cart: ' + number_of_items());
+}
+
+function number_of_items() {
+    var cnt = 0;
+
+    for(var i = 0; i < window.localStorage.length; i++){
+
+        var key = window.localStorage.key(i); // get key
+        var value = window.localStorage.getItem(key); // get value
+    
+        if(key.indexOf('product_') == 0){
+            cnt = cnt + value * 1;
+        }
+        
+    }
+    return cnt;
+    
 }
